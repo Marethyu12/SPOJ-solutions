4074307336820e2d4cfe9f4a9abbefbc7b2ee15b
Marethyu12 <42195546+Marethyu12@users.noreply.github.com>
2019-03-26 12:17:23 -0700

    Add files via upload

---
+++ ACT.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T;
+	int n;
+	
+	string s;
+	
+	cin >> T;
+	
+	while (T--)
+	{
+		cin >> n >> s;
+		
+		cout << s[s.length() - 1] << "\n";
+	}
+	
+	return 0;
+}
